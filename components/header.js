13936c3974c4401908684bca7f3ba43ec6737c78
Dan Nguyen <danthedevnguyen@gmail.com>
2022-05-21 16:53:43 -0400

    actual initial work on updated portfolio

---
+++ components/header.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import logo from "../images/icons/logo.png";
+
+export default function Navbar() {
+  const router = useRouter();
+  console.log(router.asPath);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div className="mx-auto px-2 py-6 md:py-8 float-container">
+      <div className="float-child">
+        <Link href="#header" scroll={false}>
+          <Image
+            src={logo}
+            alt="Logo"
+            width="97px"
+            height="66px"
+            layout="fixed"
+          />
+        </Link>
+      </div>
+      <div className="flex  md:flex-row justify-between float-child">
+          <Link href="#about" scroll={false}>
+            <button>
+              <a>About</a>
+            </button>
+          </Link>
+        <Link href="#experience" scroll={false}>
+          <button>
+            <a>Experience</a>
+          </button>
+        </Link>
+        <Link href="#projects" scroll={false}>
+          <button>
+            <a>Projects</a>
+          </button>
+        </Link>
+        <Link href="#contact" scroll={false}>
+          <button>
+            <a>Contact</a>
+          </button>
+        </Link>
+        <button>Resume</button>
+      </div>
+    </div>
+  );
+}
