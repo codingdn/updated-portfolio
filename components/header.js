3578c705fbe9d9f8fbf81d720e2b334e5f4a314f
Dan Nguyen <danthedevnguyen@gmail.com>
2022-05-30 00:42:09 -0400

    comment out burger menu for now

--- components/header.js
+++ components/header.js
@@ -6,76 +6,76 @@ import { slide as Menu } from "react-burger-menu";
 
 export default function Navbar() {
   return (
-    // <div className="mx-auto px-2 py-6 md:py-8 float-container-header">
-    //   <div className="float-child">
-    //     <Link href="#header" scroll={false} passHref={true}>
-    //       <Image
-    //         src={logo}
-    //         alt="Logo"
-    //         width="97px"
-    //         height="66px"
-    //         layout="fixed"
-    //       />
-    //     </Link>
-    //   </div>
-    //   <div className="flex  md:flex-row justify-between float-child">
-    //     <Link href="#about" scroll={false} passHref={true}>
-    //       <button>
-    //         <a>About</a>
-    //       </button>
-    //     </Link>
-    //     <Link href="#experience" scroll={false} passHref={true}>
-    //       <button>
-    //         <a>Experience</a>
-    //       </button>
-    //     </Link>
-    //     <Link href="#projects" scroll={false} passHref={true}>
-    //       <button>
-    //         <a>Projects</a>
-    //       </button>
-    //     </Link>
-    //     <Link href="#contact" scroll={false} passHref={true}>
-    //       <button>
-    //         <a>Contact</a>
-    //       </button>
-    //     </Link>
-    //     <button className="resume-button">
-    //       <a href="https://drive.google.com/file/d/1ks4cc_6NgRBpCGqORmz6W9LW97fbnT3c/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-    //         Resume
-    //       </a>
+    <div className="mx-auto px-2 py-6 md:py-8 float-container-header">
+      <div className="float-child">
+        <Link href="#header" scroll={false} passHref={true}>
+          <Image
+            src={logo}
+            alt="Logo"
+            width="97px"
+            height="66px"
+            layout="fixed"
+          />
+        </Link>
+      </div>
+      <div className="flex  md:flex-row justify-between float-child">
+        <Link href="#about" scroll={false} passHref={true}>
+          <button>
+            <a>About</a>
+          </button>
+        </Link>
+        <Link href="#experience" scroll={false} passHref={true}>
+          <button>
+            <a>Experience</a>
+          </button>
+        </Link>
+        <Link href="#projects" scroll={false} passHref={true}>
+          <button>
+            <a>Projects</a>
+          </button>
+        </Link>
+        <Link href="#contact" scroll={false} passHref={true}>
+          <button>
+            <a>Contact</a>
+          </button>
+        </Link>
+        <button className="resume-button">
+          <a href="https://drive.google.com/file/d/1ks4cc_6NgRBpCGqORmz6W9LW97fbnT3c/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            Resume
+          </a>
+        </button>
+      </div>
+    </div>
+    // <Menu>
+    //   <Link href="#about" scroll={false} passHref={true} className="menu-item">
+    //     <button>
+    //       <a>About</a>
     //     </button>
-    //   </div>
-    // </div>
-    <Menu>
-      <Link href="#about" scroll={false} passHref={true} className="menu-item">
-        <button>
-          <a>About</a>
-        </button>
-      </Link>
-      <Link href="#experience" scroll={false} passHref={true} className="menu-item">
-        <button>
-          <a>Experience</a>
-        </button>
-      </Link>
-      <Link href="#projects" scroll={false} passHref={true} className="menu-item">
-        <button>
-          <a>Projects</a>
-        </button>
-      </Link>
-      <Link href="#contact" scroll={false} passHref={true} className="menu-item">
-        <button> 
-          <a>Contact</a>
-        </button>
-      </Link>
-      <button>
-        <a
-          href="https://drive.google.com/file/d/1ks4cc_6NgRBpCGqORmz6W9LW97fbnT3c/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Resume
-        </a>
-      </button>
-    </Menu>
+    //   </Link>
+    //   <Link href="#experience" scroll={false} passHref={true} className="menu-item">
+    //     <button>
+    //       <a>Experience</a>
+    //     </button>
+    //   </Link>
+    //   <Link href="#projects" scroll={false} passHref={true} className="menu-item">
+    //     <button>
+    //       <a>Projects</a>
+    //     </button>
+    //   </Link>
+    //   <Link href="#contact" scroll={false} passHref={true} className="menu-item">
+    //     <button> 
+    //       <a>Contact</a>
+    //     </button>
+    //   </Link>
+    //   <button>
+    //     <a
+    //       href="https://drive.google.com/file/d/1ks4cc_6NgRBpCGqORmz6W9LW97fbnT3c/view?usp=sharing"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Resume
+    //     </a>
+    //   </button>
+    // </Menu>
   );
 }
